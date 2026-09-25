027f41a076390f10f5606e1857ab41e5b38aff4d
SaiPrateeth P <saiprateethp31@gmail.com>
2026-09-25 16:11:01 +0530

    Remove all existing files to reset project

--- script.js
+++
@@ -1,6 +0,0 @@
-const glow = document.getElementById('cursor-glow');
-if (glow) {
-  window.addEventListener('pointermove', (e) => {
-    glow.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`;
-  });
-}
